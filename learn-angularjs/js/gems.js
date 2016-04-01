3e6a1b60b705e832fbe95d3c7f9a6fff450339c9
inndee <inndeemercado@hotmail.com>
2016-04-01 18:53:01 +0800

    Added more gem data

--- learn-angularjs/js/gems.js
+++ learn-angularjs/js/gems.js
@@ -30,6 +30,50 @@
 		},
 		
 		
+		{
+			name: 'Opal' , 
+			price: 6 ,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+			canPurchase: true,
+			soldOut: false , 
+			details: 'This is the details of the product',
+			specifications: 'This is a specs of the product',
+			images: [
+				{
+					full: 'img/placeholder-full.png',
+					thumb: 'placeholder-th.jpg',
+				},
+			],
+			reviews : [
+			{
+				comments: 'This is a review of the product',
+				
+			}
+			],
+		},
+		
+		{
+			name: 'Sapphire' , 
+			price: 6 ,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+			canPurchase: true,
+			soldOut: false , 
+			details: 'This is the details of the product',
+			specifications: 'This is a specs of the product',
+			images: [
+				{
+					full: 'img/placeholder-full.png',
+					thumb: 'placeholder-th.jpg',
+				},
+			],
+			reviews : [
+			{
+				comments: 'This is a review of the product',
+				
+			}
+			],
+		},
+		
 	
 	
 	];
